38b1124a7808cf068465d6d0196bcb14ec20b440
danmxli <danmuxingli@gmail.com>
2023-10-27 11:54:17 -0400

    react-vite frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home'
+import Chat from './pages/Chat';
+
+function App() {
+
+  const pages = [
+    {
+      url: '/',
+      component: <Home />
+    },
+    {
+      url: '/chat',
+      component: <Chat />
+    }
+  ]
+
+  return (
+    <Routes>
+      {pages.map((pageData, index) => (
+        <Route key={index} path={pageData.url} element={pageData.component} />
+      ))}
+    </Routes>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import '../App.css'
+
+const Home = () => {
+    return (
+        <div className="text-blue-400">
+            Home
+        </div>
+    )
+}
+
+export default Home
