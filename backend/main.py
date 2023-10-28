922e885eaae8364bd5d7853d2dbb9aed73982248
danmxli <danmuxingli@gmail.com>
2023-10-27 22:09:26 -0400

    remove rendering templates in backend

--- backend/main.py
+++ backend/main.py
@@ -1,34 +1,31 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify
 from data.helpers import list_ids
 from data.models import get_answer
 import os
+from flask_cors import CORS
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__)
+CORS(app)
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/search', methods=["POST"])
 def search():
+    data = request.get_json()
+    user_query = data.get("query")
+
     answer_dict = {'p': [], 'a': [], 'code': []}
-    ...
-    if request.method == "POST":
-        ...
-        # get links and question ids from query
-        user_query = request.form.get("q")
-        if not user_query:
-            return render_template("search.html")
 
-        ids = list_ids(user_query)
-        
-        answer_dict = get_answer(ids)
-        print(answer_dict)
+    ids = list_ids(user_query)
+    
+    answer_dict = get_answer(ids)
 
-        # TODO render template with answer_dict
-        return render_template("search.html", query=user_query, dictionary=answer_dict)
-    return render_template("search.html")
+    return jsonify({
+        "query": user_query,
+        "response": answer_dict
+    })
 
-@app.route('/insights')
-def insights():
-    ...
-    return render_template("insights.html")
+@app.route('/', methods=["GET"])
+def read_root():
+    return jsonify({"message" : "root route reached"})
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
