c1710f6911333e7f502a91eca46e854648322d7b
danmxli <danmuxingli@gmail.com>
2023-10-28 20:44:41 -0400

    config gunicorn, vercel.json

--- frontend/src/components/DialogInterface.jsx
+++ frontend/src/components/DialogInterface.jsx
@@ -26,7 +26,7 @@ const DialogInterface = () => {
         console.log(fetchModel)
 
         try {
-            const response = await fetch('http://127.0.0.1:5000/generate', {
+            const response = await fetch('https://codebuddy-production.up.railway.app/generate', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
